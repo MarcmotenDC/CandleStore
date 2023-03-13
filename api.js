c88e4b1bd2925b7cdee9352d084fb4c477d99a29
MarcmotenDC <115433769+MarcmotenDC@users.noreply.github.com>
2023-03-12 23:15:18 -0500

    Moving code around files

--- api.js
+++ api.js
@@ -1,19 +1,31 @@
-const Commerce = require("@chec/commerce.js");
-const itemCard = document.getElementsByClassName("card")
+// const cardList = document.querySelector(".featured")
 
-const commerce = new Commerce(
-  "pk_test_508420f944b7aa39c411978846b67e228dfdfcc4a2d26", true
-);
 
-function getAllProducts() {
-  commerce.products.list().then((result) => {
-    for (let i = 0; i < result.data.length; i++) {
-      console.log(result.data[i].name)
-    } ;
- });
-}
+// function getAllProducts() {
+//   commerce.products.list().then((result) => {
+//     for (let i = 0; i < result.data.length; i++) {
+//       console.log(result.data[i].name)
+//       // const card = document.createElement('div');
+//       // card.classList = 'card';
 
-getAllProducts();
+//       // const content = `
+//       // <div class="card-container">
+//       // <div class="card">
+//       //   <img class="cardImg" src="${result.data[i].image.url}" />
+//       //   <div class="cardBot">
+//       //     <p class="cardTitle">${result.data[i].name}</p>
+//       //     <p class="cardDesc">
+//       //       ${result.data[i].description}
+//       //     </p>
+//       //     <button class="addCartBtn">Add to Cart</button>
+//       //   </div>
+//       // </div>`
+//       // cardList.innerHTML += content;
+//     } ;
+//  });
+// }
+
+// getAllProducts();
 
 
 // const APIKey = "pk_test_508420f944b7aa39c411978846b67e228dfdfcc4a2d26";
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
-const path = require('path');
+const Commerce = require("@chec/commerce.js");
+
+
 app.use(express.static(__dirname));
 
 const port = 25565
@@ -10,6 +12,14 @@ app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname,'/Homepage/index.html'))
 } )
 
+app.get('/api', (req,res) => {
+    const commerce = new Commerce(
+        "pk_test_508420f944b7aa39c411978846b67e228dfdfcc4a2d26", true
+      );
+      commerce.products.list().then((result) => {
+        res.json(result);
+       })
+})
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
 })
