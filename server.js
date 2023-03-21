804054a60529f2c9b88dedb9c617cbdc5cac4e1d
MarcmotenDC <115433769+MarcmotenDC@users.noreply.github.com>
2023-03-21 11:54:23 -0500

    removed unused function

--- server.js
+++ server.js
@@ -19,20 +19,11 @@ app.get('/api', (req,res) => {
         "pk_test_508420f944b7aa39c411978846b67e228dfdfcc4a2d26", true
       );
       commerce.products.list().then((result) => {
-        let candleName = [];
-        let candleImage = [];
         res.json(result);
-        for (i = 0; i < result.data.length; i++) {
-        candleName .push(result.data[i].name)
-        candleImage.push(result.data[i].image.url)
-        }
-        console.log(candleName)
-        console.log(candleImage)
-       })
        if (!commerce) {
         res.sendStatus(500)
        }
-})
+})})
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
 })
