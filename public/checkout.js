44f5aadfe8a860f3527718b5d306ceb87f3ab50f
MarcmotenDC <115433769+MarcmotenDC@users.noreply.github.com>
2023-04-04 19:22:12 -0500

    popup includes checkout form

--- public/checkout.js
+++ public/checkout.js
@@ -1,12 +1,14 @@
 const cartPopupBtn = document.getElementById('cartBtn');
-const cartContent = document.querySelector('#cartContent');
+const apiCartItems = document.getElementById('cartItems');
 const checkoutForm = document.getElementById('checkoutForm');
 const loadingIcon = document.querySelector('#loadingIcon');
+const closeBtn = document.querySelector('.close');
+const cartContent = document.getElementById("cartContent")
 
 cartPopupBtn.addEventListener('click', async function() {
   // Show the loading icon and hide the checkout form
   loadingIcon.style.display = 'block';
-  checkoutForm.style.display = 'none';
+  popup.style.display = 'block';
 
   // Generate HTML for the cart items
   const cartItems = await getCartItems();
@@ -14,12 +16,23 @@ cartPopupBtn.addEventListener('click', async function() {
 
   // Hide the loading icon and show the cart items and checkout form
   loadingIcon.style.display = 'none';
-  cartContent.innerHTML = cartHTML;
+  cartContent.style.display = 'block';
+  apiCartItems.style.display = 'block';
   checkoutForm.style.display = 'block';
+  apiCartItems.innerHTML = cartHTML;
 
   // Show the popup
-  popup.style.display = 'block';
+
+  closeBtn.addEventListener('click', function() {
+  console.log("closed!")
+  popup.style.display = 'none';
+  cartContent.style.display = 'none';
+  checkoutForm.style.display = 'none';
+  apiCartItems.style.display = 'none';
 });
+});
+
+
 
 async function getCartItems() {
   const res = await fetch("/cart");
