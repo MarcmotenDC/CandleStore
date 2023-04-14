22af3e089d9b9ddeff858d25617d073cb428522d
MarcmotenDC <115433769+MarcmotenDC@users.noreply.github.com>
2023-04-13 19:12:43 -0500

    renamed files to match function

--- public/cart.js
+++ public/cart.js
@@ -1,53 +1,89 @@
-const cartUrl = "/cart";
-const cartContentUrl = "/cart/contents";
-const itemCount = document.querySelector(".itemCount");
-const loadingPlaceholder = document.querySelector('.loadingPlaceholder');
-const pageContent = document.querySelector('.pageContent')
-const popupCartBtn = document.querySelector('#cartBtn');
-const popup = document.querySelector('#cart');
-document.onload = createCart();
+const cartPopupBtn = document.getElementById('cartBtn');
+const apiCartItems = document.getElementById('cartItems');
+const checkoutForm = document.getElementById('checkoutForm');
+const loadingIcon = document.querySelector('#loadingIcon');
+const closeBtn = document.querySelector('.close');
+const cartContent = document.getElementById("cartContent");
+const checkoutBtn = document.getElementById("checkoutBtn");
 
-// initializes cart
-async function createCart() {
-  const res = await fetch(cartUrl);
-  const result = await res.json(); 
-  itemCount.innerHTML = "(" + result.total_items + ")";
-  console.log("cart Initialized!");
-  loadingPlaceholder.style.display = 'none';
-  pageContent.style.display = 'block';
-}
+let cartEmpty = true;
 
-async function addToCart(event) {
-  console.log("cart event");
-  try {
-    const res = await fetch(cartUrl, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        productID: event.target.value,
-      }),
-    });
-    const renderedItemCount = await cartItems();
-    console.log("cart event done!");
-    itemCount.innerHTML = "(" + renderedItemCount + ")";
-  } catch (err) {
-    console.log(err);
-  }
-}
-// awaits fetching cart content then runs through quantity of items in cart
-async function cartItems() {
-  const res = await fetch(cartContentUrl);
+cartPopupBtn.addEventListener('click', async function() {
+  // Show the loading icon and hide the checkout form
+  loadingIcon.style.display = 'block';
+  popup.style.display = 'block';
+
+  // Generate HTML for the cart items
+  const cartItems = await getCartItems();
+  const cartHTML = generateCartHTML(cartItems);
+
+  // Hide the loading icon and show the cart items and checkout button
+  loadingIcon.style.display = 'none';
+  cartContent.style.display = 'block';
+  checkoutForm.style.display = 'none';
+  apiCartItems.style.display = 'block';
+  checkoutBtn.style.opacity = '100%'
+  apiCartItems.innerHTML = cartHTML;
+
+  // Show the popup
+
+  closeBtn.addEventListener('click', function() {
+  console.log("closed!")
+  popup.style.display = 'none';
+  cartContent.style.display = 'none';
+  checkoutForm.style.display = 'none';
+  apiCartItems.style.display = 'none';
+});
+});
+
+
+
+async function getCartItems() {
+  const res = await fetch("/cart");
   const result = await res.json();
-  let totalItems = 0;
-  //runs through array of items and adds the quantity
-  result.forEach((item) => {
-    let itemq = item.quantity;
-    totalItems = totalItems + itemq;
-  });
-  document.cookie = "itemsInCart=" + totalItems + ";path=/";
-
-  return totalItems;
+  console.log(typeof result.line_items);
+  const cartItems = result.line_items;
+  return cartItems;
 }
+
+function generateCartHTML(cartItems) {
+  if (cartItems.length === 0) {
+    cartEmpty = true;
+    checkoutBtn.style.display = 'none';
+    return "<p>Your cart is empty.</p>";
+  }
+
+  let cartHTML = "";
+
+  // Iterate through each item in the cart
+  for (const itemID in cartItems) {
+    const item = cartItems[itemID];
+    const itemHTML = `
+      <div class="cartItem">
+        <h3 class="itemName">${item.name}: ${item.quantity}</h3>
+      </div>
+    `;
+    cartHTML += itemHTML;
+  }
+  cartEmpty = false;
+  checkoutBtn.style.display = 'inline-block';
+  return cartHTML;
+}
+
+
+// Checkout Function
+checkoutBtn.addEventListener("click", async () => {
+  loadingIcon.style.display = 'block';
+  checkoutBtn.style.opacity = '0%'
+  try {
+    const res = await fetch('/checkout');
+    const data = await res.json();
+    loadingIcon.style.display = 'none';
+    checkoutBtn.style.display = 'none';
+    checkoutForm.style.display = 'block';
+    apiCartItems.style.display = 'none';
+  } catch(err) {
+    console.log(err)
+  }
+
+});
