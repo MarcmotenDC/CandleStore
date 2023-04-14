1fa0964ae72a1ae341e534f2beb76befed3ceb20
MarcmotenDC <115433769+MarcmotenDC@users.noreply.github.com>
2023-04-14 12:10:56 -0500

    displays price, adds variables to localstorage

--- public/cart.js
+++ public/cart.js
@@ -1,89 +1,99 @@
-const cartPopupBtn = document.getElementById('cartBtn');
-const apiCartItems = document.getElementById('cartItems');
-const checkoutForm = document.getElementById('checkoutForm');
-const loadingIcon = document.querySelector('#loadingIcon');
-const closeBtn = document.querySelector('.close');
+const cartPopupBtn = document.getElementById("cartBtn");
+const apiCartItems = document.getElementById("cartItems");
+const checkoutForm = document.getElementById("checkoutForm");
+const loadingIcon = document.querySelector("#loadingIcon");
+const closeBtn = document.querySelector(".close");
 const cartContent = document.getElementById("cartContent");
 const checkoutBtn = document.getElementById("checkoutBtn");
+const totalPriceHTML = document.getElementById("totalPrice");
 
 let cartEmpty = true;
+let totalPrice = 0;
 
-cartPopupBtn.addEventListener('click', async function() {
+// get Items stored in cart on API
+async function getCartItems() {
+  const res = await fetch("/cart");
+  const result = await res.json();
+  // logs line_items
+  console.log(result.line_items);
+  const cartItems = result.line_items;
+  // Stores line_items in localstorage for sending order data
+  localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  return cartItems;
+}
+
+// Display cart information in popup
+function generateCartHTML(cartItems) {
+  // when cart is empty this shows
+  if (cartItems.length === 0) {
+    cartEmpty = true;
+    checkoutBtn.style.display = "none";
+    return "<p>Your cart is empty.</p>";
+  }
+
+  let cartHTML = "";
+  totalPrice = 0;
+  // Iterate through each item in the cart
+  for (const itemID in cartItems) {
+    const item = cartItems[itemID];
+    const itemHTML = `
+      <div class="cartItem">
+        <h3 class="itemName">${item.name}: ${item.quantity} | $${item.price.raw}</h3>
+      </div>
+    `;
+    cartHTML += itemHTML;
+    // adds price of all items to var
+    totalPrice = totalPrice + item.price.raw;
+  }
+// sets displayed total
+  totalPriceHTML.innerHTML = `Total: $`+totalPrice;
+  // sets the cart total to be pulled in checkout.js
+  localStorage.setItem("totalPrice", totalPrice);
+  cartEmpty = false;
+  // shows checkout button ONLY when there are items in the cart
+  checkoutBtn.style.display = "inline-block";
+  return cartHTML;
+}
+
+// show Cart popup
+cartPopupBtn.addEventListener("click", async function () {
   // Show the loading icon and hide the checkout form
-  loadingIcon.style.display = 'block';
-  popup.style.display = 'block';
+  loadingIcon.style.display = "block";
+  popup.style.display = "block";
 
   // Generate HTML for the cart items
   const cartItems = await getCartItems();
   const cartHTML = generateCartHTML(cartItems);
 
   // Hide the loading icon and show the cart items and checkout button
-  loadingIcon.style.display = 'none';
-  cartContent.style.display = 'block';
-  checkoutForm.style.display = 'none';
-  apiCartItems.style.display = 'block';
-  checkoutBtn.style.opacity = '100%'
+  loadingIcon.style.display = "none";
+  cartContent.style.display = "block";
+  checkoutForm.style.display = "none";
+  apiCartItems.style.display = "block";
+  checkoutBtn.style.opacity = "100%";
   apiCartItems.innerHTML = cartHTML;
 
-  // Show the popup
-
-  closeBtn.addEventListener('click', function() {
-  console.log("closed!")
-  popup.style.display = 'none';
-  cartContent.style.display = 'none';
-  checkoutForm.style.display = 'none';
-  apiCartItems.style.display = 'none';
+  // Close popup when X is clicked
+  closeBtn.addEventListener("click", function () {
+    popup.style.display = "none";
+    cartContent.style.display = "none";
+    checkoutForm.style.display = "none";
+    apiCartItems.style.display = "none";
+  });
 });
-});
-
-
-
-async function getCartItems() {
-  const res = await fetch("/cart");
-  const result = await res.json();
-  console.log(typeof result.line_items);
-  const cartItems = result.line_items;
-  return cartItems;
-}
-
-function generateCartHTML(cartItems) {
-  if (cartItems.length === 0) {
-    cartEmpty = true;
-    checkoutBtn.style.display = 'none';
-    return "<p>Your cart is empty.</p>";
-  }
-
-  let cartHTML = "";
-
-  // Iterate through each item in the cart
-  for (const itemID in cartItems) {
-    const item = cartItems[itemID];
-    const itemHTML = `
-      <div class="cartItem">
-        <h3 class="itemName">${item.name}: ${item.quantity}</h3>
-      </div>
-    `;
-    cartHTML += itemHTML;
-  }
-  cartEmpty = false;
-  checkoutBtn.style.display = 'inline-block';
-  return cartHTML;
-}
-
 
 // Checkout Function
 checkoutBtn.addEventListener("click", async () => {
-  loadingIcon.style.display = 'block';
-  checkoutBtn.style.opacity = '0%'
+  // shows loading icon when api is being called
+  loadingIcon.style.display = "block";
+  checkoutBtn.style.opacity = "0%";
   try {
-    const res = await fetch('/checkout');
-    const data = await res.json();
-    loadingIcon.style.display = 'none';
-    checkoutBtn.style.display = 'none';
-    checkoutForm.style.display = 'block';
-    apiCartItems.style.display = 'none';
-  } catch(err) {
-    console.log(err)
+    // removes loading icon and displays checkout form
+    loadingIcon.style.display = "none";
+    checkoutBtn.style.display = "none";
+    checkoutForm.style.display = "block";
+    apiCartItems.style.display = "none";
+  } catch (err) {
+    console.log(err);
   }
-
 });
