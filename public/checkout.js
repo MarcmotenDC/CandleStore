89a7688b30de70731ed2ccfab54e54aed8039e56
MarcmotenDC <115433769+MarcmotenDC@users.noreply.github.com>
2023-04-13 20:38:22 -0500

    rewrote form/ added checkout function

---
+++ public/checkout.js
@@ -0,0 +1,62 @@
+function submitPayment() {
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    const email = document.getElementById("email").value;
+    const street = document.getElementById("street").value;
+    const city = document.getElementById("city").value;
+    const region = document.getElementById("region").value;
+    const postalCode = document.getElementById("postalCode").value;
+    const country = document.getElementById("country").value;
+    const cardNum = document.getElementById("cardNum").value;
+    const expiryMonth = document.getElementById("expiryMonth").value;
+    const expiryYear = document.getElementById("expiryYear").value;
+    const cvv = document.getElementById("cvv").value;
+  
+    const paymentData = {
+      amount: 1000, // Change this to your desired amount
+      payment: {
+        gateway: "test_gateway",
+        card: {
+          number: cardNum,
+          expiry_month: expiryMonth,
+          expiry_year: expiryYear,
+          cvc: cvv,
+          postal_code: postalCode
+        }
+      }
+    };
+  
+    const shippingData = {
+      name: firstName + " " + lastName,
+      street: street,
+      town_city: city,
+      county_state: region,
+      postal_zip_code: postalCode,
+      country: country,
+      email: email
+    };
+  
+    const orderData = {
+      line_items: [
+        // Add your line items here
+      ],
+      shipping: shippingData
+    };
+  
+    fetch("/checkout", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ paymentData, orderData })
+    })
+    .then(response => response.json())
+    .then(data => {
+      // Handle the response from your server
+      console.log(data);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  }
+  
